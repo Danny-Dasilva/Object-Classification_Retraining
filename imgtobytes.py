b0c00e9eede01f7ae2ed2ea04c1d7f8453cf0aa1
Danny-Dasilva.com <yahchayildasilva@gmail.com>
2019-10-14 18:04:12 -0400

    added bytes conversion

--- imgtobytes.py
+++ imgtobytes.py
@@ -1,9 +1,25 @@
 from PIL import Image
 import io
 
-im = Image.open('index.jpeg')
+im = Image.open('test.png')
 im_resize = im.resize((500, 500))
 buf = io.BytesIO()
-im_resize.save(buf, format='JPEG')
+im.save(buf, format='PNG')
 byte_im = buf.getvalue()
 print(byte_im)
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import urllib.request
+import io
+import binascii
+
+
+stream = io.BytesIO(byte_im)
+
+img = Image.open(stream)
+draw = ImageDraw.Draw(img)
+font = ImageFont.truetype("arial.ttf",14)
+draw = ImageDraw.Draw(img)
+img.save("a_test.png")
