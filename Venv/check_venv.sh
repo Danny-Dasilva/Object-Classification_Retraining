acb2dc811f4f0beb3e94872233a9167bb5821369
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-10-15 15:11:41 -0400

    added venv.sh

---
+++ Venv/check_venv.sh
@@ -0,0 +1,48 @@
+
+#!/bin/bash
+BSHRC=~/.bashrc
+
+
+until [ "${obs_venv}" == "$obs_input" ] ; do
+    venvdir=$(which virtualenvwrapper.sh)
+    source $venvdir
+    workon $obs_input
+    if  [ "$VIRTUAL_ENV" != "" ]
+    then
+        venv_pth=$VIRTUAL_ENV
+        obs_venv=${venv_pth##*/}
+            if  [ "${obs_venv}" == "$obs_input" ]
+            then 
+                echo "functional"
+                echo ${obs_venv}
+                if grep -q "export obs_venv" "$BSHRC";
+                    then
+                        echo "Overwriding previous obs_venv"
+                        grep -v "export obs_venv" $BSHRC > temp && mv temp $BSHRC
+                        echo "export obs_venv="$obs_venv"" >> $BSHRC 
+                    else
+                        echo "export obs_venv="$obs_venv"" >> $BSHRC
+                fi
+                break
+            else
+                echo "deactivating"
+                deactivate
+                mkvirtualenv $obs_input
+            fi
+    else
+        echo "not in venv"
+        my_dir=`dirname $0`
+        #Call the other script
+        sh ./Venv/create_virtualenv.sh
+       #source "$mydir"/create_virtualenv.sh
+        echo "create virtualenv"
+        mkvirtualenv $obs_input
+        echo "mkvirtualenv env"
+    fi
+done
+
+echo "WORKING IN OBJECT_DETECTION"
+
+
+
+
---
+++ Venv/create_virtualenv.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sudo pip3 install virtualenv
+
+sudo pip3 install virtualenvwrapper
+
+BSHRC=~/.bashrc
+
+
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+
+export WORKON_HOME=~/.Envs
+
+
+if grep -q "WORKON_HOME" "$BSHRC";
+    then
+        echo "Files are here"
+    else
+        echo "Writing ENVS folder"
+        mkdir -p $WORKON_HOME
+
+        echo "exporting env variables to bashrc"
+        
+        echo 'WORKON_HOME=~/.Envs' >> $BSHRC
+        echo 'VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3' >> $BSHRC
+        echo 'source ~/.local/bin/virtualenvwrapper.sh' >> $BSHRC
+fi
+
+venvdir=$(which virtualenvwrapper.sh)
+source $venvdir
+
+
