6dea5485a062c38823deda178a0e8a8e96daef62
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-11-09 21:24:07 +0000

    updated

--- imgtobytes.py
+++ imgtobytes.py
@@ -6,11 +6,12 @@ im_resize = im.resize((500, 500))
 buf = io.BytesIO()
 im.save(buf, format='PNG')
 byte_im = buf.getvalue()
-print(byte_im)
+print(len(byte_im))
 
 
 from PIL import Image
 import io
+print(len(byte_im))
 stream = io.BytesIO(byte_im)
 img = Image.open(stream)
 img.save("a_test.png")
